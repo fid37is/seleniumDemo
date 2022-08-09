d165e4d789c88933b0b66c7637e444b3332b3488
Agba Fidelis <el-fido@Agbas-MacBook-Pro.local>
2022-08-09 17:13:11 +0100

    JS test library, chai

--- tests/firstTest.js
+++ tests/firstTest.js
@@ -1,11 +1,13 @@
 const { By, Key} = require("selenium-webdriver");
 const {Builder} = require("selenium-webdriver");
+const assert = require("assert");
+var should = require('chai').should();
+
 
 async function testing() {
 
     //Launch browser
     let driver = await new Builder().forBrowser("firefox").build();
-
 
     //Navigate to application
     await driver.get("https://lambdatest.github.io/sample-todo-app/");
@@ -15,6 +17,17 @@ async function testing() {
     //Add a todo
     await driver.findElement(By.id("sampletodotext")).sendKeys("First selenium", Key.RETURN);
 
+
+    //Assert 
+    let todoText = await driver.findElement(By.xpath("//li[last()]")).getText().then(function(value){
+        return value;
+    });
+
+    //assert using node assertion
+    assert.strictEqual(todoText, "First selenium");
+
+    //assert using chai should
+    todoText.should.equal("First selenium");
 
     //Exit browser
     await driver.quit();
